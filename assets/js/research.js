798232542e4484e7b7f5f9ad0ae9ca53c0c2b18d
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2021-04-10 14:13:41 -0400

    research accordion

--- assets/js/research.js
+++ assets/js/research.js
@@ -15,6 +15,33 @@ $(document).ready(function(){
 
         // Your Code To Process Output Goes here
         console.log(json);
+        let heading = "<h3>Research</h3>";
+        let div = "<div>Faculty Research: <ul>";
+        $("#research").append(heading);
+        $.each(json.byFaculty, function(index, research) {
+            div = div + "<li>" + research.facultyName + "<br>" + research.username + "<br>";
+            $.each(research.citations, function(index1,citation) {
+                div = div + citation + "<br>";
+            })
+            div = div + "</li>";
+        })
+        div = div + " </ul>Interest Area: <ul>"
+        $.each(json.byInterestArea, function(index, area) {
+            console.log(area);
+            div = div + "<li>" + area.areaName + "<br>";
+            $.each(area.citations, function(index2,citation) {
+                div = div + citation + "<br>";
+            })
+            div = div + "</li>";
+        })
+        div = div + "</div>";
+        $("#research").append(div);
+        
+        $("#research").accordion({
+            collapsible: true,
+            active: false,
+            heightStyle: "content"
+        });
 
     }).fail (function(jqXHR) {
         // Consider using the jQueryUI "Dialog" widget to display errors
