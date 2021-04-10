6f7c2a3840339773dbd9eaa5a028cb2828fbe25d
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2021-04-10 18:14:53 -0400

    finsihed work for monday except for the problem w graudate degrees that im not sure how to fix yet

--- assets/js/degrees.js
+++ assets/js/degrees.js
@@ -19,8 +19,10 @@ $(document).ready(function(){
 
         let heading1;
         let div1;
+        let heading2;
+        let div2;
+
         $.each(json.undergraduate, function(index1,degree1) {
-            console.log(degree1);
             heading1 = "<h3>" + degree1.title + "</h3>";
             div1 = "<div>" + degree1.description + "<br>Concentrations: </ul>";
             $.each(degree1.concentrations, function(index2,concentration) {
@@ -29,17 +31,11 @@ $(document).ready(function(){
             div1 = div1 + "</ul>";
             $("#undergrad").append(heading1);
             $("#undergrad").append(div1);
-            $("#undergrad").accordion({
-                collapsible: true,
-                active: false,
-                heightStyle: "content"
-            });
+           
         })
-
-        let heading2 = "<h3>Graduate Degrees</h3>";
-        let div2;
+        
+        //problem here bc last graduate entry doesnt have a title and is a certificate
         $.each(json.graduate, function(index2,degree2) {
-            console.log(degree2);
             heading2 = "<h3>" + degree2.title + "</h3>";
             div2 = "<div>" + degree2.description + "<br>Concentrations: </ul>";
             $.each(degree2.concentrations, function(index2,concentration) {
@@ -47,30 +43,25 @@ $(document).ready(function(){
             })
             div2 = div2 + "</ul>";
             $("#undergrad").append(heading2);
-            $("#undergrad").append(div2);
-            $("#undergrad").accordion({
-                collapsible: true,
-                active: false,
-                heightStyle: "content"
-            });
+            $("#undergrad").append(div2);   
         })
+        $("#grad").accordion({
+            collapsible: true,
+            active: false,
+            heightStyle: "content"
+        });
+        
+        $("#undergrad").accordion({
+            collapsible: true,
+            active: false,
+            heightStyle: "content"
+        });
 
+        
     }).fail (function(jqXHR) {
         // Consider using the jQueryUI "Dialog" widget to display errors
         $('#degrees').append(jqXHR.responseText);
     });
 
-    //json format
-    //undergrad
-        //title
-        //degree name
-        //description
-        //concentrations (array)
-            //key and value
-    //grad
-        //title
-        //degree name
-        //description
-        //concentrations (array)
-            //key and value
+
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -15,6 +15,10 @@ $(document).ready(function(){
 
         // Your Code To Process Output Goes here
         console.log(json);
+        $('#about').append("<h1>" + json.title + "</h1>" +
+        "<h3>" + json.description + "</h3>" + 
+        "<h4>'" + json.quote + "' -" + json.quoteAuthor + "</h4>");
+
 
     }).fail (function(jqXHR) {
         // Consider using the jQueryUI "Dialog" widget to display errors
