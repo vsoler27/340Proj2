9b70a13e516ad69b122f4d3e9a5a0c4489ff7c0a
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2021-04-10 11:08:51 -0400

    deleted html files

--- assets/js/minors.js
+++ assets/js/minors.js
@@ -29,3 +29,35 @@ getData({path:'/minors/'}).done(function(json){
         });
     });
 });
+
+$(document).ready(function(){
+        
+    let ugDegrees = underGrad.undergraduate;
+
+    let degreeDetails;
+    let concentrations;
+    for (let i = 0; i < ugDegrees.length; i++ ){
+        $("#ugAccordion").append("<h3>" + ugDegrees[i].degreeName.toUpperCase() + "</h3>")
+            degreeDetails = "<div>" +
+                ugDegrees[i].title + "<br>" +
+                ugDegrees[i].description + "<br>";
+
+                concentrations = "<br>Concentrations:"
+                concentrations = concentrations + "<ul>";
+                for (let j = 0; j < ugDegrees[i].concentrations.length; j++) {
+                    concentrations = concentrations +
+                    "<li>" + ugDegrees[i].concentrations[j] + "</li>"
+                }
+            concentrations = concentrations + "</ul>";
+            degreeDetails = degreeDetails + concentrations;
+        $("#ugAccordion").append(
+            degreeDetails
+        )                        
+    }
+
+    $("#ugAccordion").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content"
+    });
+});
