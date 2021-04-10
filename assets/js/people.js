4d176b470d1ab52b94c966fa4e7ac4f722406aae
Da7238 <60263576+Da7238@users.noreply.github.com>
2021-04-09 22:53:25 -0400

    Added code to fetch faculty data from JSON file

---
+++ assets/js/people.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	function getData(pathName){
+        return $.ajax({
+        type: 'get',
+        url:'http://solace.ist.rit.edu/~plgics/proxy.php',
+        dataType:'json',
+        data: pathName,
+        cache:false,
+        async:true
+        })
+    }
+
+    getData({path:'/people/faculty/'}).done(function(data){
+
+        // Your Code To Process Output Goes here
+        console.log(data)
+
+    }).fail (function(jqXHR) {
+        // Consider using the jQueryUI "Dialog" widget to display errors
+        $('#content').append(jqXHR.responseText);
+    });
+})
