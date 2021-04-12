67eee9c8bd4bc4b266a819ac4085e56098a696d2
Darlene Ardila <da7238@rit.edu>
2021-04-11 21:45:00 -0400

    Added some tabs to courses.

--- assets/js/courses.js
+++ assets/js/courses.js
@@ -14,22 +14,28 @@ $(document).ready(function(){
     getData({path:'/courses/'}).done(function(json){
 
         // Your Code To Process Output Goes here
-        //console.log(json);
-        let minCourses = json;
+        console.log(json);
 
             let tabNames = "<ul>";
             let divs ='';
-            for (let i = 0; i < minCourses.courses; i++ ){
-                tabNames = tabNames + "<li><a href='#divID" + i + "'>" + minCourses[i].courseID + "</a></li>";
+            $.each(json, function(index, course) {
+                tabNames = tabNames + "<li><a href='#divID"
+                    + index + "'>"
+                    + course.degreeName + "</a></li>";
+                divs = divs + course.semester;
+                $.each(course.courses, function (index2, courseInfo) {
+                    divs = divs + "<div id=divID" + index2 + ">"
+                        + courseInfo.semester + "<br>" +
+                        courseInfo.description + "</div>";
+                })
 
-                divs = divs + "<div id=divID" + i + ">" +  minCourses[i].title + "<br>" +
-                    minCourses[i].description + "</div>";
-            }
+                
+            })
             tabNames = tabNames + "</ul>";
-            $( "#courses" ).append(tabNames);
-            $( "#courses" ).append(divs);
+            $("#courses").append(tabNames);
+            $("#courses").append(divs);
 
-            $( "#courses" ).tabs();
+            $("#courses").tabs();
 
     }).fail (function(jqXHR) {
         // Consider using the jQueryUI "Dialog" widget to display errors
