ff0ee44f1b3fc5a3c387642fcb514b3979dec187
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2021-04-11 22:15:44 -0400

    removing course.js

--- assets/js/research.js
+++ assets/js/research.js
@@ -15,6 +15,38 @@ $(document).ready(function(){
         
         //console.log(json);
 
+        //tab layout
+       /* getData({path:'/courses/'}).done(function(json){
+
+            // Your Code To Process Output Goes here
+            console.log(json);
+    
+                let tabNames = "<ul>";
+                let divs ='';
+                $.each(json, function(index, course) {
+                    tabNames = tabNames + "<li><a href='#divID"
+                        + index + "'>"
+                        + course.degreeName + "</a></li>";
+                    divs = divs + course.semester;
+                    $.each(course.courses, function (index2, courseInfo) {
+                        divs = divs + "<div id=divID" + index2 + ">"
+                            + courseInfo + "</div>";
+                    })
+    
+                    
+                })
+                tabNames = tabNames + "</ul>";
+                $("#courses").append(tabNames);
+                $("#courses").append(divs);
+    
+                $("#courses").tabs();
+    
+        }).fail (function(jqXHR) {
+            // Consider using the jQueryUI "Dialog" widget to display errors
+            $('#courses').append(jqXHR.responseText);
+        });
+    })*/
+
         
 /*
         // Your Code To Process Output Goes here
