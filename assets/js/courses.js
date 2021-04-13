f740a37d42bd0cf3ca44b3c8c7aff58008e8eb70
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2021-04-13 15:11:52 -0400

    added courses accordion

---
+++ assets/js/courses.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    function getData(pathName){
+        return $.ajax({
+        type: 'get',
+        url:'http://solace.ist.rit.edu/~plgics/proxy.php',
+        dataType:'json',
+        data: pathName,
+        cache:false,
+        async:true
+        })
+    }
+
+    //accordion layout pls!!!!
+    getData({path:'/courses/'}).done(function(json){
+        //console.log(json);
+        let heading;
+        let divs;
+        $.each(json,function(index,degree) {
+            heading = "<h4>" + degree.degreeName + "</h4>";
+            divs = "<div>" + degree.semester + "<br>";
+            $.each(degree.courses, function(index2,course) {
+                divs = divs + course + "<br>";
+            })
+            divs = divs + "</div>";
+            $('#courses').append(heading);
+            $('#courses').append(divs);
+        })
+        $("#courses").accordion({
+            collapsible: true,
+            active: false,
+            heightStyle: "content"
+        });
+    })
+})
