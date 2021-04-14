f622513d49efc8044ba31dee5b8ff6e5d9a5fed2
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2021-04-14 13:08:10 -0400

    grid stuff

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -13,37 +13,23 @@ var countries = [
     { Name: "United Kingdom", Id: 3 }
 ];
 
-$(function() {
- 
-    $("#jsGrid").jsGrid({
-        height: "80%",
-        width: "100%",
- 
-        filtering: true,
-        editing: true,
-        sorting: true,
-        paging: true,
-        autoload: true,
- 
-        pageSize: 15,
-        pageButtonCount: 5,
- 
-        controller: db,
- 
-        fields: [
-            { name: "Name", type: "text", width: 150 },
-            { name: "Age", type: "number", width: 50 },
-            { name: "Address", type: "text", width: 200 },
-            { name: "Country", type: "select", items: db.countries, valueField: "Id", textField: "Name" },
-            { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
-            { type: "control", modeSwitchButton: false, editButton: false }
-        ]
-    });
- 
- 
-    $(".config-panel input[type=checkbox]").on("click", function() {
-        var $cb = $(this);
-        $("#jsGrid").jsGrid("option", $cb.attr("id"), $cb.is(":checked"));
-    });
- 
+$("#jsGrid").jsGrid({
+    width: "100%",
+    height: "400px",
+
+    inserting: true,
+    editing: true,
+    sorting: true,
+    paging: true,
+
+    data: clients,
+
+    fields: [
+        { name: "Name", type: "text", width: 150, validate: "required" },
+        { name: "Age", type: "number", width: 50 },
+        { name: "Address", type: "text", width: 200 },
+        { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
+        { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+        { type: "control" }
+    ]
 });
