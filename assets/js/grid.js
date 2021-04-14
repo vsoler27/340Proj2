6ed7d84cba5f141fca057e748d8d3b8df5441578
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2021-04-14 14:16:58 -0400

    attempt number one at the table

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,35 +1,36 @@
-var clients = [
-    { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
-    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
-    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
-    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
-    { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
-];
+$(document).ready(function(){
+    function getData(pathName){
+        return $.ajax({
+        type: 'get',
+        url:'http://solace.ist.rit.edu/~plgics/proxy.php',
+        dataType:'json',
+        data: pathName,
+        cache:false,
+        async:true
+        })
+    }
 
-var countries = [
-    { Name: "", Id: 0 },
-    { Name: "United States", Id: 1 },
-    { Name: "Canada", Id: 2 },
-    { Name: "United Kingdom", Id: 3 }
-];
-
-$("#jsGrid").jsGrid({
-    width: "100%",
-    height: "400px",
-
-    inserting: true,
-    editing: true,
-    sorting: true,
-    paging: true,
-
-    data: clients,
-
-    fields: [
-        { name: "Name", type: "text", width: 150, validate: "required" },
-        { name: "Age", type: "number", width: 50 },
-        { name: "Address", type: "text", width: 200 },
-        { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
-        { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
-        { type: "control" }
-    ]
-});
+    getData({path:'/employment/coopTable/'}).done(function(json) {
+        console.log(json.coopTable.coopInformation);
+        $("#jsGrid").jsGrid({
+            width: "100%",
+            height: "400px",
+        
+            inserting: true,
+            editing: true,
+            sorting: true,
+            paging: true,
+        
+            
+            data: json.coopTable.coopInformation,
+        
+            fields: [
+                { name: "Employer", type: "text", width: 150 },
+                { name: "Degree", type: "text", width: 100 },
+                { name: "City", type: "text", width: 100 },
+                { name: "Term", type: "text", width:100 },
+                { type: "control" }
+            ]
+        });
+    })
+})
